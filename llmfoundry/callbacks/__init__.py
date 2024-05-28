1d494983da112a5674a9369874807f69cfbdd14f
JackZ-db <jack.zhang@databricks.com>
2024-05-27 22:26:03 -0700

    testing new import for straggler class

--- llmfoundry/callbacks/__init__.py
+++ llmfoundry/callbacks/__init__.py
@@ -11,8 +11,9 @@ from composer.callbacks import (
     OptimizerMonitor,
     RuntimeEstimator,
     SpeedMonitor,
-    GlobalStragglerDetector,
 )
+
+from composer.callbacks.global_straggler_detector_draft import GlobalStragglerDetector
 
 from llmfoundry.callbacks.async_eval_callback import AsyncEval
 from llmfoundry.callbacks.curriculum_learning_callback import CurriculumLearning
